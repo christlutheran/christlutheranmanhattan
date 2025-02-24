b2e9219ad83ec1c33e540f9633cede25bacf0e6c
Bryce Johnston <bjohnstondev@gmail.com>
2025-02-24 11:20:32 -0600

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,8 +20,7 @@ export default defineConfig({
   },
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp',
-      formats: ['jpeg', 'png']
+      entrypoint: 'astro/assets/services/sharp'
     }
   },
   markdown: {
