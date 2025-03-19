7e52e76901b62962b64236d77da09023268e82ff
Bryce Johnston <bjohnstondev@gmail.com>
2025-03-19 14:34:13 -0500

    update links

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -8,22 +8,22 @@ export const navItems: NavItem[] = [
   { title: 'Home', path: '/' },
   { 
     title: 'About', 
-    path: '/about',
+    path: '/about/',
     children: [
-      { title: 'About Us', path: '/about' },
-      { title: 'Beliefs', path: '/beliefs' },
-      { title: 'Divine Service', path: '/divine-service' },
-      { title: 'Our Pastor', path: '/our-pastor' },
-      { title: 'Service Times', path: '/service-times' },
+      { title: 'About Us', path: '/about/' },
+      { title: 'Beliefs', path: '/beliefs/' },
+      { title: 'Divine Service', path: '/divine-service/' },
+      { title: 'Our Pastor', path: '/our-pastor/' },
+      { title: 'Service Times', path: '/service-times/' },
     ]
   },
   {
     title: 'Resources',
-    path: '/resources',
+    path: '/resources/',
     children: [
-      { title: 'Luther\'s Small Catechism', path: '/catechism' },
-      { title: 'Lutheran Resources', path: '/resources' }
+      { title: 'Luther\'s Small Catechism', path: '/catechism/' },
+      { title: 'Lutheran Resources', path: '/resources/' }
     ]
   },
-  { title: 'Contact', path: '/contact' }
+  { title: 'Contact', path: '/contact/' }
 ];
