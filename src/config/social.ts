18a63017812ac0eefd0705fd4a10af4087f707f0
Bryce Johnston <bjohnstondev@gmail.com>
2025-06-15 22:15:08 -0500

    fix sermons page

--- src/config/social.ts
+++ src/config/social.ts
@@ -3,5 +3,9 @@ export const socialConfig = {
   facebook: {
     url: 'https://www.facebook.com/people/Christ-Lutheran-Church-Missouri-Synod/100064606092871/',
     label: 'Facebook'
+  },
+  youtube: {
+    url: 'https://www.youtube.com/@ChristLutheranManhattan',
+    label: 'YouTube'
   }
 } as const;
